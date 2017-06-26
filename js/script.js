1c4b621a2d67f8aa73349bf87471a2f76dbb8d20
kervin5 <kervinlvh@gmail.com>
2017-06-25 22:22:44 -0700

    Fix consecutive operators

--- js/script.js
+++ js/script.js
@@ -1,46 +1,47 @@
 specialChar = ["0", "00"];
-operationChar = ['.', "DEL", "*", "-", "+", "="];
+operationChar = ['.', "DEL", "*", "-", "+", "=","/"];
 
-$(document).ready(function() {
-  opScreen = $("#display p");
-  resScreen = $("#results p");
-  
-  $("#numbers .button").click(function() {
-    keyVal = $(this).children("p").text(); // Gets the key value
-    currentValue = opScreen.text();
+$(document).ready(function () {
+   var opScreen = $("#display p");
+    var resScreen = $("#results p");
 
-    if (currentValue === "0") {
-      if (!specialChar.includes(keyVal) && !operationChar.includes(keyVal)) {
-        opScreen.text(keyVal);
-      }
-    } else {
-      if (currentValue.length <= 10) {
-        if (keyVal === "." && currentValue.includes(".")) {
-          opScreen.text(currentValue);
+    $("#numbers .button").click(function () {
+        keyVal = $(this).children("p").text(); // Gets the key value
+        currentValue = opScreen.text();
+
+        if (currentValue === "0") {
+            if (!specialChar.includes(keyVal) && !operationChar.includes(keyVal)) {
+                opScreen.text(keyVal);
+            }
         } else {
-          opScreen.text(currentValue + keyVal);
+            if (currentValue.length <= 10) {
+                if (keyVal === "." && currentValue.includes(".")) {
+                    opScreen.text(currentValue);
+                } else {
+                    opScreen.text(currentValue + keyVal);
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  $("#operators .button").click(function() {
-    currentValue = opScreen.text();
-    keyVal = $(this).children("p").text();
+    $("#operators .button").click(function () {
+        currentValue = opScreen.text();
+        keyVal = $(this).children("p").text();
 
-    if (keyVal === "=") {
-      resScreen.text(eval(currentValue));
-    } else if (keyVal === "DEL") {
-      resScreen.text("0");
-      opScreen.text("0");
-    } else {
-      if (currentValue.length <= 9) {
-        if (currentValue[currentValue.length - 1] !== keyVal) {
-          opScreen.text(currentValue + keyVal);
+        if (keyVal === "=") {
+            resScreen.text(eval(currentValue));
+        } else if (keyVal === "DEL") {
+            resScreen.text("0");
+            opScreen.text("0");
         } else {
-          opScreen.text(currentValue);
+            if (currentValue.length <= 9) {
+                var lastDigit = currentValue.substring(currentValue.length - 1);
+                if (!operationChar.includes(lastDigit)) {
+                    opScreen.text(currentValue + keyVal);
+                } else {
+                    opScreen.text(currentValue);
+                }
+            }
         }
-      }
-    }
-  });
+    });
 });
